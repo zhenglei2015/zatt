2d270211c41edbd926ffa7466d51f176c9076ccd
Simone Accascina <simon@accascina.me>
2016-03-18 23:47:10 +0100

    Add logging

---
+++ zatt/server/logger.py
@@ -0,0 +1,29 @@
+import logging
+import asyncio
+from datetime import datetime
+from .config import fetch_config
+
+
+def tick():
+    logger.debug('Tick: ' + datetime.now().isoformat('T'))
+    loop = asyncio.get_event_loop()
+    loop.call_later(1, tick)
+
+config = fetch_config()
+if config['debug']:
+    formatter_patten = '%(message)s'
+    logLevel = logging.DEBUG
+    loop = asyncio.get_event_loop()
+    loop.call_later(1, tick)
+else:
+    logLevel = logging.INFO
+    formatter_patten = '%(asctime)s %(message)s'
+
+# extra = {'app_name':config['id']}
+logger = logging.getLogger(__name__)
+syslog = logging.StreamHandler()
+formatter = logging.Formatter(formatter_patten)
+syslog.setFormatter(formatter)
+logger.setLevel(logLevel)
+logger.addHandler(syslog)
+# logger = logging.LoggerAdapter(logger, extra)
