0bd292d7d806922e52f8b7ea6ab5190a4d3f4140
Simone Accascina <simon@accascina.me>
2016-04-12 15:50:29 +0200

    Fix indentation

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup, find_packages
 
 
 exclude = ['docs', 'tests']
-entry_points = {}
+
 if version.major < 3 or (version.major == 3 and version.minor < 4):
     exclude += ['zatt.server']
+    entry_points = {}
 else:
-    entry_points.update({'console_scripts':['zattd=zatt.server.main:run']})
+    entry_points = {'console_scripts':['zattd=zatt.server.main:run']}
 
 setup(name='Zatt',
       version='1.0',
@@ -18,27 +19,27 @@ setup(name='Zatt',
       author_email='simon@accascina.me',
       url='https://github.com/simonacca/zatt',
       license='GPL',
-      keywords = 'distributed consensus raft',
+      keywords='distributed consensus raft',
       classifiers=[
-        'Development Status :: 3 - Alpha',
+          'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Distributed Systems :: Consens Algorithms',
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Distributed Systems :: Consens Algorithms',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL License',
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: GPL License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
       ],
-    # packages = ['zatt.client'],
-    packages = find_packages(exclude=exclude),
-    entry_points = entry_points,
+      # packages = ['zatt.client'],
+      packages=find_packages(exclude=exclude),
+      entry_points=entry_points,
      )
